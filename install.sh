953ff3ff89e9e1d5df7b0adb9a6631b0ca5f2236
TechXero <steve@techxero.com>
2023-05-21 22:27:29 +0300

    Font

--- install.sh
+++ install.sh
@@ -74,8 +74,8 @@ if ! pacman -Qs noto-fonts-emoji; then
   sudo pacman -S noto-fonts-emoji --noconfirm
 fi
 
-if ! pacman -Qs nerd-fonts-hasklig; then
-  sudo pacman -S nerd-fonts-hasklig --noconfirm
+if ! pacman -Qs otf-hasklig-nerd; then
+  sudo pacman -S otf-hasklig-nerd --noconfirm
 fi
 sleep 2
 echo
