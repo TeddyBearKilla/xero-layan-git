ebc664b2a766a8686ee8a6c7d84d6eccc2dc070f
TechXero <steve@techxero.com>
2023-05-21 14:55:21 +0300

    Update

--- install.sh
+++ install.sh
@@ -4,28 +4,64 @@ echo "##########################################"
 echo "Be Careful this will override your Rice!! "
 echo "##########################################"
 sleep 5
-echo "Creating Backups of ~/.config folder"
+echo
+echo "Creating Backup of ~/.config folder"
 echo "#####################################"
 cp -r ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
 sleep 2
-echo "Removing old Latte-Dock & Lightly"
+echo
+echo "Installing Necessary Packages    "
 echo "#################################"
-sudo pacman -R --noconfirm lightly-git
-sudo pacman -R --noconfirm latte-dock
+# Check if lightly-git and latte-dock exist
+if pacman -Qs lightly-git && pacman -Qs latte-dock; then
+  # Remove lightly-git and latte-dock
+  sudo pacman -Rns lightly-git latte-dock --noconfirm
+  
+  # Install latte-dock-git and kvantum
+  sudo pacman -S latte-dock-git kvantum --noconfirm
+fi
 sleep 2
 echo
-echo "Installing New latte-Git version"
-echo "#################################"
-sudo pacman -S --needed --noconfirm latte-dock-git kvantum
-sleep 2
 echo "Installing Layan Theme"
 echo "#################################"
-sudo pacman -S --needed --noconfirm layan-gtk-theme-git layan-kde-git tela-purple-icon-theme-git
+# Check and install theme packages if they don't exist
+if ! pacman -Qs layan-gtk-theme-git; then
+  sudo pacman -S layan-gtk-theme-git --noconfirm
+fi
+
+if ! pacman -Qs layan-kde-git; then
+  sudo pacman -S layan-kde-git --noconfirm
+fi
+
+if ! pacman -Qs tela-circle-icon-theme-purple-git; then
+  sudo pacman -S tela-circle-icon-theme-purple-git --noconfirm
+fi
 sleep 2
+echo
 echo "Installing Fonts"
 echo "#################################"
-sudo pacman -S --needed --noconfirm ttf-hack-nerd ttf-fira-code ttf-meslo-nerd-font-powerlevel10k ttf-terminus-nerd noto-fonts-emoji
+# Install font packages if they don't exist
+if ! pacman -Qs ttf-hack-nerd; then
+  sudo pacman -S ttf-hack-nerd --noconfirm
+fi
+
+if ! pacman -Qs ttf-fira-code; then
+  sudo pacman -S ttf-fira-code --noconfirm
+fi
+
+if ! pacman -Qs ttf-meslo-nerd-font-powerlevel10k; then
+  sudo pacman -S ttf-meslo-nerd-font-powerlevel10k --noconfirm
+fi
+
+if ! pacman -Qs ttf-terminus-nerd; then
+  sudo pacman -S ttf-terminus-nerd --noconfirm
+fi
+
+if ! pacman -Qs noto-fonts-emoji; then
+  sudo pacman -S noto-fonts-emoji --noconfirm
+fi
 sleep 2
+echo
 echo "Applying new Rice, hold on..."
 echo "#################################"
 cp -Rf Configs/Home/. ~
@@ -33,10 +69,12 @@ sudo cp -Rf Configs/System/. /
 sed -i "s/{.*}/{C1}/g" ~/XeroAscii
 sudo sed -i "s/Current=.*/Current=XeroDark/" /etc/sddm.conf.d/kde_settings.conf
 sleep 2
+echo
 echo "Applying Flatpak Theme Fix..."
 echo "#################################"
-cp -r /usr/share/themes/Layan-Dark ~/.themes/
+sh /usr/local/bin/stylepak install-system
 sleep 2
+echo
 echo "Applying Grub Theme...."
 echo "#################################"
 chmod +x CyberRe.sh
@@ -44,5 +82,6 @@ sudo ./CyberRe.sh
 sudo sed -i "s/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1920x1080/g" /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sleep 2
-echo "RebootTo Apply Settings..."
+echo
+echo "Plz Reboot To Apply Settings..."
 echo "#################################"
