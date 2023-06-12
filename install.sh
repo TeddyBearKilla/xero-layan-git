baa6c06b2e91aec7108a898c194321cb67793a82
TechXero <steve@techxero.com>
2023-06-13 00:43:13 +0300

    Install

--- install.sh
+++ install.sh
@@ -41,8 +41,8 @@ sleep 2
 echo
 echo "Applying Grub Theme...."
 echo "#######################"
-chmod +x CyberRe.sh
-sudo ./CyberRe.sh
+chmod +x Grub.sh
+sudo ./Grub.sh
 sudo sed -i "s/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1920x1080/g" /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sleep 2
